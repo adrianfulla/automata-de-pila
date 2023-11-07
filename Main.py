a371ed285dfcf336c7755d2376a3a6205e8b09c3
Adrian Fulladolsa <ful21592@uvg.edu.gt>
2023-11-06 20:38:56 -0600

    Creacion de automata

--- Main.py
+++ Main.py
@@ -10,10 +10,31 @@ Fecha: 6/10/2023
 class automata_de_pila:
     def __init__(self, Q, E, P, S, Z, d, F):
         self.Q = Q # Conjunto de estados
-        self.E = E # Simbolos de entrada
+        self.E = E # Alfabeto de entrada
         self.P = P # Simbolos de pila
         self.S = S # Estado inicial
         self.Z = Z # Simbolo inicial de pila
-        self.d = d # Subset finito
-        self.F = F # Funcion
+        self.d = d # Funcion de transición
+        self.F = F # Estados finales
         
+        
+def run_automata():
+    Q = ['q0', 'q1', 'q2']
+    E = [0,1]
+    P = ['X', 'Z0']
+    F=['q2']
+    d={'q0': {0: {'Z0':['q0', 'XXZ0'], 'X':['q0', 'XX']},
+                    1: {'X': ['q1', 'e']}
+            },
+           'q1': {1: {'X': ['q1', 'e']}, 'e':['q2','Z0']
+            }
+        }
+    S= 'q0'
+    Z = 'Z0'
+
+    auto = automata_de_pila(Q, E, P, S, Z, d, F)
+
+    
+    
+run_automata()
+    
